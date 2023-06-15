252ca4ba94a2bed3b50c5159968a0e4a447fb123
mariaadannies <mariaadannies26@gmail.com>
2023-06-15 19:07:33 +0700

    fix entry model

--- calories/models.py
+++ calories/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Entry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
-    time = models.TimeField(a)
+    time = models.TimeField(auto_now_add=True)
     text = models.CharField(max_length=255)
     calories = models.IntegerField(null=True, blank=True)
     total_calories = models.BooleanField(default=False)
