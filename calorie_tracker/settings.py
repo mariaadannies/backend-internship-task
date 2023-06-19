2e3270fb4b1dc03d98b78dbf71f12b1a73409889
Maria Aurellia <mariaadannies26@gmail.com>
2023-06-19 20:24:18 +0700

    fix requirement

--- calorie_tracker/settings.py
+++ calorie_tracker/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'calories',
-     'rest_framework_simplejwt',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
